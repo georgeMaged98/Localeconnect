dfdcf4b0e04122e4706f3b9bd8ba722e430c0dd0
Maha Marhag <maha.merhag@gmail.com>
2024-01-07 18:41:56 +0100

    feat(LSSA-41): add test

--- LocaleConnectBackend/itinerary/src/main/java/com/localeconnect/app/itinerary/service/ItineraryService.java
+++ LocaleConnectBackend/itinerary/src/main/java/com/localeconnect/app/itinerary/service/ItineraryService.java
@@ -16,20 +16,20 @@ public class ItineraryService {
     private final WebClient webClient;
 
     public Itinerary createItinerary( @Valid ItineraryDTO itineraryDTO, String userId) {
-        Itinerary itinerary = Itinerary.builder()
-                .name(itineraryDTO.getName())
-                .description(itineraryDTO.getDescription())
-                .numberOfDays(itineraryDTO.getNumberOfDays())
-                .dailyActivities(itineraryDTO.getDailyActivities())
-                .placesToVisit(itineraryDTO.getPlacesToVisit())
-                .tags(itineraryDTO.getTags())
-                .imageUrls(itineraryDTO.getImageUrls()).build();
 
         // Make a synchronous request to the userService and save the itinerary if userId matches
         Boolean check = this.webClient.get()
                 .uri("http://localhost:8080/api/user/verifyUser/{userId}", userId)
                 .retrieve().bodyToMono(Boolean.class).block();
         if (Boolean.TRUE.equals(check)) {
+            Itinerary itinerary = Itinerary.builder()
+                    .name(itineraryDTO.getName())
+                    .description(itineraryDTO.getDescription())
+                    .numberOfDays(itineraryDTO.getNumberOfDays())
+                    .dailyActivities(itineraryDTO.getDailyActivities())
+                    .placesToVisit(itineraryDTO.getPlacesToVisit())
+                    .tags(itineraryDTO.getTags())
+                    .imageUrls(itineraryDTO.getImageUrls()).build();
             return itineraryRepository.save(itinerary);
 
         } else {
