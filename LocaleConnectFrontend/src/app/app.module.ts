ef0fa794ae0fef65fc38147eb5513c49ada30dc7
Maha Marhag <marhag@telum.biz>
2024-01-17 19:07:38 +0100

    add core and routing

--- LocaleConnectFrontend/src/app/app.module.ts
+++ LocaleConnectFrontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CoreModule} from "./core/core.module";
+import {LandingPageModule} from "./pages/landing-page/landing-page.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    LandingPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
