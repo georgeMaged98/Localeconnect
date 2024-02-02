17b371d63c9668d4c90e19271e6dc0a81b86d7a7
George Elfayoumi <g.maged327@gmail.com>
2024-02-03 00:22:39 +0100

    gcp storage initial setup

---
+++ LocaleConnectBackend/gcp-storage/src/main/java/gcpstorage/GcpConfig.java
@@ -0,0 +1,33 @@
+package gcpstorage;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Configuration
+public class GcpConfig {
+
+    @Value("${gcp.credentials.path}")
+    private String credentialsPath;
+
+    @Bean
+    public Storage googleCloudStorage() throws IOException {
+
+        FileInputStream serviceAccountStream = new FileInputStream(credentialsPath);
+        // Authenticate with Google Cloud Storage using the service account key
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream);
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+        // Create a Storage client using default credentials
+//        Storage storage = StorageOptions.getDefaultInstance().getService();
+        return storage;
+    }
+}
+
