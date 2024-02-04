b94b1762037f638f7fb3fd3843a50f7c2d7e69aa
Maha Marhag <maha.merhag@gmail.com>
2024-02-04 15:20:14 +0100

    add notifications

--- LocaleConnectFrontend/src/app/core/header/detail-header.component.ts
+++ LocaleConnectFrontend/src/app/core/header/detail-header.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WebSocketService} from "../../service/web-socket.service";
+import {Notification} from "../../model/notification";
 
 @Component({
   selector: 'app-detail-header',
   templateUrl: './detail-header.component.html',
-  styleUrls: ['./detail-header.component.scss']
+  styleUrls: ['./detail-header.component.scss'],
 })
-export class DetailHeaderComponent {
+export class DetailHeaderComponent implements OnInit {
+  notifications: Notification[] = [];
+  showNotifications: boolean = false;
+  hasNewNotifications: boolean= false;
 
-  //TODO: open user profile
-  openAccountMenu() {
+  constructor(private webSocketService: WebSocketService) {
   }
+
+  ngOnInit() {
+    this.webSocketService.getNotifications().subscribe((notifications) => {
+      this.notifications = notifications;
+      this.hasNewNotifications = notifications.length > 0;
+
+    });
+  }
+
+  toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+    this.hasNewNotifications = false;
+
+  }
+
 }
