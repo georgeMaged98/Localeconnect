470d0d53bfca307416d8badeef746b0e3d57ef70
Moetaz Khelil <moetazkhelil@yahoo.com>
2024-02-05 23:37:37 +0100

    add error handling for User and refactor code

--- LocaleConnectBackend/user/src/main/java/com/localeconnect/app/user/controller/UserController.java
+++ LocaleConnectBackend/user/src/main/java/com/localeconnect/app/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.localeconnect.app.user.dto.TravelerDTO;
 import com.localeconnect.app.user.dto.UserDTO;
 import com.localeconnect.app.user.exception.UserAlreadyExistsException;
 import com.localeconnect.app.user.exception.UserDoesNotExistException;
+import com.localeconnect.app.user.response_handler.ResponseHandler;
 import com.localeconnect.app.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -24,91 +25,56 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    public ResponseEntity<Object> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, users, null);
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<?> getUserById(@PathVariable("userId") Long userId) {
-        try {
-            return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
-        }
-        catch (UserDoesNotExistException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        catch (Exception e) {
-            return new ResponseEntity<>("An internal error has occured", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Object> getUserById(@PathVariable("userId") Long userId) {
+        UserDTO user = userService.getUserById(userId);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, user, null);
     }
 
-    @PostMapping("/register/traveler")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?> registerTraveler(@RequestBody TravelerDTO travelerDTO) {
-        try {
-            return new ResponseEntity<>(userService.registerTraveler(travelerDTO), HttpStatus.CREATED);
-        } catch (UserAlreadyExistsException | IllegalArgumentException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            return new ResponseEntity<>("An internal error has  occured", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PostMapping("/register-traveler")
+    public ResponseEntity<Object> registerTraveler(@RequestBody TravelerDTO travelerDTO) {
+        TravelerDTO registeredTraveler = userService.registerTraveler(travelerDTO);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, registeredTraveler, null);
     }
-    @PostMapping("/register/localguide")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?> registerLocalGuide(@RequestBody LocalguideDTO localguideDTO) {
-        try {
-            return new ResponseEntity<>(userService.registerLocalguide(localguideDTO), HttpStatus.CREATED);
-        } catch (UserAlreadyExistsException | IllegalArgumentException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            return new ResponseEntity<>("An internal error has  occured", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+    @PostMapping("/register-localguide")
+    public ResponseEntity<Object> registerLocalGuide(@RequestBody LocalguideDTO localguideDTO) {
+        LocalguideDTO registeredLocalguide = userService.registerLocalguide(localguideDTO);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, registeredLocalguide, null);
     }
+
     @PutMapping("/update")
-    public ResponseEntity<?> updateUser(@RequestBody UserDTO userDTO) {
-        try {
-            return new ResponseEntity<>(userService.updateUser(userDTO), HttpStatus.OK);
-        }
-        catch(UserDoesNotExistException | IllegalArgumentException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            return new ResponseEntity<>("An internal error has  occured", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Object> updateUser(@RequestBody UserDTO userDTO) {
+        UserDTO updatedUser = userService.updateUser(userDTO);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, updatedUser, null);
     }
+
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable("userId") Long userId) {
-        try {
-            userService.deleteUser(userId);
-            return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
-        } catch (UserDoesNotExistException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            log.error("Error deleting user: {}", e.getMessage());
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Object> deleteUser(@PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, null, null);
     }
+
     @PostMapping("/{userId}/follow/{followerId}")
-    public ResponseEntity<?> followUser(@PathVariable("userId") Long userId, @PathVariable("followerId") Long followerId) {
-        try {
-            userService.followUser(userId, followerId);
-            return new ResponseEntity<>("Follow request successful", HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("Error following user: {}", e.getMessage());
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<Object> followUser(@PathVariable("userId") Long userId, @PathVariable("followerId") Long followerId) {
+        userService.followUser(userId, followerId);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, null, null);
     }
 
     @PostMapping("/{userId}/unfollow/{followeeId}")
-    public ResponseEntity<?> unfollowUser(@PathVariable("userId") Long userId, @PathVariable("followeeId") Long followeeId) {
-        try {
-            userService.unfollowUser(userId, followeeId);
-            return new ResponseEntity<>("Unfollow successful", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<Object> unfollowUser(@PathVariable("userId") Long userId, @PathVariable("followeeId") Long followeeId) {
+        userService.unfollowUser(userId, followeeId);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, null, null);
     }
+
     @GetMapping("/exists/{userId}")
-    public ResponseEntity<Boolean> checkUserExists(@PathVariable("userId") Long userId) {
+    public ResponseEntity<Object> checkUserExists(@PathVariable("userId") Long userId) {
         boolean exists = userService.checkUserId(userId);
-        return new ResponseEntity<>(exists, HttpStatus.OK);
+        return ResponseHandler.generateResponse("Success!", HttpStatus.OK, exists, null);
     }
 }
