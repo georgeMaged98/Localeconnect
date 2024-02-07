0681845ddf0ce8054d136acb01cffc752cc642c5
Maha Marhag <marhag@telum.biz>
2024-02-07 13:27:36 +0100

    add route guards

--- LocaleConnectFrontend/src/app/app-routing.module.ts
+++ LocaleConnectFrontend/src/app/app-routing.module.ts
@@ -5,27 +5,33 @@ import {ItineraryComponent} from "./pages/itinerary/itinerary.component";
 import {FeedComponent} from "./pages/feed/feed.component";
 import {MeetupComponent} from "./pages/meetup/meetup.component";
 import {GuideComponent} from "./pages/guide/guide.component";
-
+import {authGuard} from "./service/auth-guard";
+//TODO: uncomment guard
 const routes: Routes = [
   {
     path: '',
     component:LandingPageComponent
   },{
   path: 'pages/itineraries',
-    component: ItineraryComponent
+    component: ItineraryComponent,
+
+//    canActivate:[authGuard]
   },
   {
     path: 'pages/home',
-    component: FeedComponent
+    component: FeedComponent,
+   // canActivate:[authGuard]
   },
   {
     path: 'pages/meetups',
-    component: MeetupComponent
+    component: MeetupComponent,
+    // canActivate:[authGuard]
   },
 
   {
     path: 'pages/guides',
-    component: GuideComponent
+    component: GuideComponent,
+    // canActivate:[authGuard]
   }
 ];
 
