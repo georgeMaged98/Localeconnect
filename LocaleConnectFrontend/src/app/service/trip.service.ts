ad825cdeb3af4ae2e0f6386cbe08ece7c9f9e3da
Maha Marhag <marhag@telum.biz>
2024-02-08 17:30:29 +0100

    add trip component and service

---
+++ LocaleConnectFrontend/src/app/service/trip.service.ts
@@ -0,0 +1,47 @@
+// trip.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Trip} from "../model/trip";
+import {Itinerary} from "../model/itinerary";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+  private apiUrl = 'http://localhost:8080/api/trip';
+  private tripSource = new BehaviorSubject<Trip | null>(null);
+  currentTrip = this.tripSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+  changeTrip(trip: any) {
+    if(trip){
+      this.tripSource.next(trip);
+
+    }
+  }
+  createTrip(trip: Trip): Observable<Trip> {
+    return this.http.post<Trip>(`${this.apiUrl}/create`, trip);
+  }
+
+  getAllTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.apiUrl}/all`);
+  }
+
+  getTripById(tripId: number): Observable<Trip> {
+    return this.http.get<Trip>(`${this.apiUrl}/${tripId}`);
+  }
+
+  updateTrip(tripId: number, trip: Trip): Observable<Trip> {
+    return this.http.put<Trip>(`${this.apiUrl}/update/${tripId}`, trip);
+  }
+
+  deleteTrip(tripId: number): Observable<{}> {
+    return this.http.delete(`${this.apiUrl}/delete/${tripId}`);
+  }
+
+  searchTrip(tripName: string): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.apiUrl}/search?name=${tripName}`);
+  }
+
+}
