30650a86fe36f44b1b3589f6ad10c95cb6e6aef2
Maha Marhag <marhag@telum.biz>
2024-02-10 13:38:56 +0100

    fetch current user

--- LocaleConnectFrontend/src/app/service/auth-interceptor.ts
+++ LocaleConnectFrontend/src/app/service/auth-interceptor.ts
@@ -1,22 +1,20 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse
-} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {Router} from '@angular/router';
 import {AuthService} from "./auth.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router, private authenticationService: AuthService) {}
+  constructor(private router: Router, private authenticationService: AuthService) {
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const currentUser = this.authenticationService.currentUserValue;
-    const token = currentUser?.token;
+    const token = localStorage.getItem('token');
 
-    if (currentUser && token) {
+    if (token) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
@@ -24,10 +22,11 @@ export class AuthInterceptor implements HttpInterceptor {
       });
     }
 
+
     return next.handle(request).pipe(catchError(error => {
       if (error instanceof HttpErrorResponse && error.status === 401) {
         this.authenticationService.logout();
-        this.router.navigate(['']).then(r => r= false);
+        this.router.navigate(['']).then(r => r = false);
       }
       return throwError(error);
     }));
