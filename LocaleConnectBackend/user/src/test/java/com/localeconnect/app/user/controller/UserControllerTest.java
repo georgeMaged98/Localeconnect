845769fefc0c5060c29e56e94a3988b5bbfb4998
Moetaz Khelil <moetazkhelil@yahoo.com>
2024-02-11 19:51:25 +0100

    add UserControllerTest

--- LocaleConnectBackend/user/src/test/java/com/localeconnect/app/user/controller/UserControllerTest.java
+++ LocaleConnectBackend/user/src/test/java/com/localeconnect/app/user/controller/UserControllerTest.java
@@ -1,170 +1,118 @@
 package com.localeconnect.app.user.controller;
 
-import com.localeconnect.app.user.auth.AuthenticationResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.localeconnect.app.user.dto.UserDTO;
-import com.localeconnect.app.user.model.User;
-import com.localeconnect.app.user.service.AuthenticationService;
 import com.localeconnect.app.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
-    @InjectMocks
-    private UserController userController;
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
 
     @Mock
     private UserService userService;
 
+    @InjectMocks
+    private UserController userController;
+
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-    @Test
-    void testGetUserById() {
-        Long userId = 1L;
-        UserDTO mockUser = new UserDTO();
-        mockUser.setId(userId);
-        mockUser.setUserName("Test");
-        mockUser.setEmail("Test1@example.com");
-        mockUser.setFirstName("Test");
-        mockUser.setLastName("Last");
-
-        when(userService.getUserById(userId)).thenReturn(mockUser);
-
-        ResponseEntity<Object> response = userController.getUserById(userId);
-
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        UserDTO actualResponseObject = (UserDTO) responseBody.get("data");
-
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        assertEquals(userId, actualResponseObject.getId());
-        assertEquals("Test", actualResponseObject.getUserName());
-        assertEquals("Test1@example.com", actualResponseObject.getEmail());
-        assertEquals("Test", actualResponseObject.getFirstName());
-        assertEquals("Last", actualResponseObject.getLastName());
-
-        verify(userService, times(1)).getUserById(userId);
+        objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        mockMvc = MockMvcBuilders.standaloneSetup(userController)
+                .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+                .build();
     }
 
     @Test
-    void testGetAllUsers() {
-        List<UserDTO> mockUsers = new ArrayList<>();
-        // User1
-        Long userId = 1L;
-        UserDTO mockUser1 = new UserDTO();
-        mockUser1.setId(userId);
-        mockUser1.setUserName("Test1");
-        mockUser1.setEmail("Test1@example.com");
-        mockUser1.setFirstName("Test1");
-        mockUser1.setLastName("Last1");
+    void getAllUsers_ShouldReturnUsers() throws Exception {
+        List<UserDTO> users = List.of(
+                UserDTO.builder().id(1L).firstName("Alice").lastName("Smith").userName("aliceSmith").email("alice@example.com").build(),
+                UserDTO.builder().id(2L).firstName("Bob").lastName("Jones").userName("bobbyJ").email("bob@example.com").build()
+        );
+        given(userService.getAllUsers()).willReturn(users);
 
-        // User2
-        Long secondUserId = 2L;
-        UserDTO mockUser2 = new UserDTO();
-        mockUser2.setId(secondUserId);
-        mockUser2.setUserName("Test2");
-        mockUser2.setEmail("Test2@example.com");
-        mockUser2.setFirstName("Test2");
-        mockUser2.setLastName("Last2");
-
-        mockUsers.add(mockUser1);
-        mockUsers.add(mockUser2);
-
-        when(userService.getAllUsers()).thenReturn(mockUsers);
-        ResponseEntity<Object> response = userController.getAllUsers();
-
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        List<UserDTO> actualResponseObject = (List<UserDTO>) responseBody.get("data");
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        assertEquals(2, actualResponseObject.size());
-        assertEquals(mockUsers.get(0).getEmail(), actualResponseObject.get(0).getEmail());
-        assertEquals(mockUsers.get(1).getEmail(), actualResponseObject.get(1).getEmail());
-
-        verify(userService, times(1)).getAllUsers();
-    }
-    @Test
-    void testUpdateUser() {
-        UserDTO mockUserToUpdate = new UserDTO();
-        mockUserToUpdate.setId(1L);
-        mockUserToUpdate.setUserName("UpdatedUser");
-        mockUserToUpdate.setEmail("updateduser@example.com");
-        mockUserToUpdate.setFirstName("Updated");
-        mockUserToUpdate.setLastName("User");
-
-        when(userService.updateUser(any(UserDTO.class))).thenReturn(mockUserToUpdate);
-
-        ResponseEntity<Object> response = userController.updateUser(mockUserToUpdate);
-        Map<String, Object> responseBody = (Map<String, Object>) response.getBody();
-        UserDTO actualResponseObject = (UserDTO) responseBody.get("data");
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-
-        assertEquals("UpdatedUser", actualResponseObject.getUserName());
-        assertEquals("updateduser@example.com", actualResponseObject.getEmail());
-
-        verify(userService, times(1)).updateUser(any(UserDTO.class));
+        mockMvc.perform(get("/api/user/secured/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data[0].id").value(users.get(0).getId()))
+                .andExpect(jsonPath("$.data[0].firstName").value("Alice"))
+                .andExpect(jsonPath("$.data[0].lastName").value("Smith"))
+                .andExpect(jsonPath("$.data[0].userName").value("aliceSmith"))
+                .andExpect(jsonPath("$.data[0].email").value("alice@example.com"))
+                .andExpect(jsonPath("$.data[1].id").value(users.get(1).getId()))
+                .andExpect(jsonPath("$.data[1].firstName").value("Bob"))
+                .andExpect(jsonPath("$.data[1].lastName").value("Jones"))
+                .andExpect(jsonPath("$.data[1].userName").value("bobbyJ"))
+                .andExpect(jsonPath("$.data[1].email").value("bob@example.com"));
     }
 
     @Test
-    void testDeleteUser() {
-        Long userIdToDelete = 1L;
-        doNothing().when(userService).deleteUser(userIdToDelete);
-        ResponseEntity<Object> response = userController.deleteUser(userIdToDelete);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+    void getUserById_ShouldReturnUser() throws Exception {
+        UserDTO userDTO = UserDTO.builder().id(1L).firstName("Charlie").lastName("Brown").userName("charlieB").email("charlie@example.com").build();
+        given(userService.getUserById(1L)).willReturn(userDTO);
 
-        verify(userService, times(1)).deleteUser(userIdToDelete);
-    }
-    @Test
-    void testFollowUser() {
-        Long followerId = 1L;
-        Long userIdToFollow = 2L;
-
-        doNothing().when(userService).followUser(followerId, userIdToFollow);
-
-        ResponseEntity<Object> response = userController.followUser(followerId, userIdToFollow);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        verify(userService, times(1)).followUser(followerId, userIdToFollow);
+        mockMvc.perform(get("/api/user/secured/{userId}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data.id").value(1L))
+                .andExpect(jsonPath("$.data.firstName").value("Charlie"))
+                .andExpect(jsonPath("$.data.lastName").value("Brown"))
+                .andExpect(jsonPath("$.data.userName").value("charlieB"))
+                .andExpect(jsonPath("$.data.email").value("charlie@example.com"));
     }
 
     @Test
-    void testUnfollowUser() {
-        Long followerId = 1L;
-        Long userIdToUnfollow = 2L;
+    void updateUser_ShouldReturnUpdatedUser() throws Exception {
+        UserDTO userDTO = UserDTO.builder()
+                .firstName("TestFirstName")
+                .lastName("TestLastName")
+                .userName("TestUserName")
+                .email("test@example.com")
+                .password("TestPassword") // Note: Passwords are usually not returned in responses for security reasons
+                .dateOfBirth(LocalDate.now().minusYears(20))
+                .build();
+        given(userService.updateUser(any(UserDTO.class))).willReturn(userDTO);
 
-        doNothing().when(userService).unfollowUser(followerId, userIdToUnfollow);
-
-        ResponseEntity<Object> response = userController.unfollowUser(followerId, userIdToUnfollow);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        verify(userService, times(1)).unfollowUser(followerId, userIdToUnfollow);
+        mockMvc.perform(put("/api/user/secured/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.firstName").value("TestFirstName"))
+                .andExpect(jsonPath("$.data.lastName").value("TestLastName"))
+                .andExpect(jsonPath("$.data.userName").value("TestUserName"))
+                .andExpect(jsonPath("$.data.email").value("test@example.com"))
+                .andExpect(jsonPath("$.data.dateOfBirth").value(userDTO.getDateOfBirth().toString())); // Comparing date as string
     }
 
+    @Test
+    void deleteUser_ShouldReturnSuccess() throws Exception {
+        doNothing().when(userService).deleteUser(anyLong());
 
+        mockMvc.perform(delete("/api/user/secured/delete/{userId}", 1))
+                .andExpect(status().isOk());
+    }
 }
